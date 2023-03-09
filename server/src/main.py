98a6a3229ee689c0c39d00dac64aa17abd4f8f46
OtavioFSantos <otaviofs01@gmail.com>
2023-03-09 00:59:59 -0300

    add rest of functions

--- server/src/main.py
+++ server/src/main.py
@@ -18,7 +18,7 @@ class Main:
             json.dump(self.users, file)
     
     def signup(self, email, password):
-        self.users[email] = {'Password': password, 'Messages': []}
+        self.users[email] = {'Password': password, 'Emails': []}
         self.save_users()
         return Response('Success', 'Registration successful.')
     
@@ -29,3 +29,40 @@ class Main:
             return Response(type='Error', message='Incorrect password.')
         elif password == self.users[email]['Password']:
             return Response(type='Success', message='Login successful.')
+    
+    def mailbox(self, email):
+        if self.users[email]['Emails'] == []:
+            return Response(type='Error', message='There are no messages.')
+        data = ''
+        for i in range(len(self.users[email]['Emails'])):
+            data += (f'{i+1} - Subject: {self.users[email]["Emails"][i]["Subject"]}\n')
+        return Response(type='Success', message='', data=data)
+    
+    def send(self, sender, rcv, subject, body):
+        if rcv not in self.users: 
+            return Response(type='Error', message='Invalid receiver.')
+        self.users[rcv]['Emails'].append({'From': str(sender), 'Subject': str(subject), 'Body': str(body)})
+        self.save_users()
+        return Response(type='Success', message='Email sent successfully.')
+    
+    def open(self, email, id_msg):
+        if not self.users[email]['Emails']:
+            return Response(type='Error', message='You have no messages.')
+        return Response(type='Success', message='', data=
+        f'From: {self.users[email]["Emails"][int(id_msg)]["From"]}\n'
+        f'Subject: {self.users[email]["Emails"][int(id_msg)]["Subject"]}\n'
+        f'Body: {self.users[email]["Emails"][int(id_msg)]["Body"]}')
+    
+    def delete(self, email, id_msg):
+        if not self.users[email]['Emails']:
+            return Response(type='Error', message='You have no messages.')
+        self.users[email]['Emails'].pop(int(id_msg))
+        self.save_users()
+        return Response(type='Success', message='Email deleted successfully.')
+    
+    def clear(self, email):
+        if not self.users[email]['Emails']:
+            return Response(type='Error', message='You have no messages.')
+        self.users[email]['Emails'] = []
+        self.save_users()
+        return Response(type='Success', message='Mailbox cleared.')
