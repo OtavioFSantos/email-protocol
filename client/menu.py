5955a3a1f6bba2d35d4bef0c14b329c7862759a2
OtavioFSantos <otaviofs01@gmail.com>
2023-03-08 21:30:17 -0300

    add menu

---
+++ client/menu.py
@@ -0,0 +1,11 @@
+def MenuLogin():
+    print('\n1 - Login')
+    print('2 - Registrar-se')
+
+def MenuMaster():
+    print('\n1 - Caixa de entrada')
+    print('2 - Ver mensagem')
+    print('3 - Enviar mensagem')
+    print('4 - Deletar mensagem')
+    print('5 - Limpar caixa de entrada')
+    print('6 - Logout')
